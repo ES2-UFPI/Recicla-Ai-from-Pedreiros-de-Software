0a1dbe33b00e7070a38ba11531e54998fcedfd97
Joao Carlos <joaokarlos1951@gmail.com>
2025-10-16 08:51:51 -0300

    feat: roteamento telas

---
+++ src/screens/OptionsScreen.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function OptionsScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Tela de Opções</Text>
+    </View>
+  );
+}

a18e961e773ba49017535f34d8b469313ef0afb7
JBernardoFortes <joseberk2@gmail.com>
2025-11-10 07:50:31 -0300

    #66 feat: Create route to inventory screen

--- src/navigation/AppNavigation.tsx
+++ src/navigation/AppNavigation.tsx
@@ -5,6 +5,7 @@ import LoginScreen from '../screens/LoginScreen';
 import PasswordScreen from '../screens/PasswordScreen';
 import BottomTabs from './BottomTabs';
 import LocationPickerScreen from '../screens/LocationPickerScreen';
+import { InventoryScreen } from '../screens/InventoryScreen';
 import MapScreen from '@/screens/MapScreen';
 import CollectionPointMapScreen from '@/screens/CollectionPointMapScreen';
 import CollectionPointsListScreen from '@/screens/CollectionPointsListScreen';
@@ -12,18 +13,19 @@ import CollectionPointsListScreen from '@/screens/CollectionPointsListScreen';
 const Stack = createNativeStackNavigator();
 
 export default function AppNavigation() {
-    return (
-        <NavigationContainer>
-            <Stack.Navigator initialRouteName="Login" screenOptions={{ headerShown: true }}>
-                <Stack.Screen name="Login" component={LoginScreen} />
-                <Stack.Screen name="Register" component={RegisterScreen} />
-                <Stack.Screen name="Password" component={PasswordScreen} />
-                <Stack.Screen name="Home" component={BottomTabs} />
-                <Stack.Screen name="Map" component={MapScreen} />
-                <Stack.Screen name="LocationPicker" component={LocationPickerScreen} />
-                <Stack.Screen name="CollectionPointMap" component={CollectionPointMapScreen} />
-                <Stack.Screen name="CollectionPointsList" component={CollectionPointsListScreen} />
-            </Stack.Navigator>
-        </NavigationContainer>
-    );
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login" screenOptions={{ headerShown: true }}>
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Register" component={RegisterScreen} />
+        <Stack.Screen name="Password" component={PasswordScreen} />
+        <Stack.Screen name="Home" component={BottomTabs} />
+        <Stack.Screen name="Inventory" component={InventoryScreen} />
+        <Stack.Screen name="Map" component={MapScreen} />
+        <Stack.Screen name="LocationPicker" component={LocationPickerScreen} />
+        <Stack.Screen name="CollectionPointMap" component={CollectionPointMapScreen} />
+        <Stack.Screen name="CollectionPointsList" component={CollectionPointsListScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
--- src/navigation/BottomTabs.tsx
+++ src/navigation/BottomTabs.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { Home, Settings, History, User } from 'lucide-react-native';
+import { Home, Settings, History, User, Backpack } from 'lucide-react-native';
 
 import HomeScreen from '../screens/HomeScreen';
 import OptionsScreen from '../screens/OptionsScreen';
@@ -11,7 +11,6 @@ const Tab = createBottomTabNavigator();
 
 export default function BottomTabs() {
   return (
-    
     <Tab.Navigator
       screenOptions={({ route }) => ({
         headerShown: false,
@@ -27,16 +26,19 @@ export default function BottomTabs() {
               return <History color={color} size={size} />;
             case 'Perfil':
               return <User color={color} size={size} />;
+            case 'Inventário':
+              return <Backpack color={color} size={size} />;
             default:
               return null;
           }
         },
-      })}
-    >
+      })}>
       <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Inventário" component={OptionsScreen} />
       <Tab.Screen name="Opções" component={OptionsScreen} />
       <Tab.Screen name="Histórico" component={HistoryScreen} />
       <Tab.Screen name="Perfil" component={ProfileScreen} />
+
     </Tab.Navigator>
   );
 }
